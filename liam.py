71fece410011e39caaa91b1457a7b3be7e2bf942
greenbean1 <beauhoover1@gmail.com>
2016-01-09 08:38:56 +0000

    added more

--- liam.py
+++ liam.py
@@ -1,15 +1,15 @@
 from bs4 import BeautifulSoup
 import urllib.request
-import re
 
 
 LIAM_NEESON_WIKIPEDIA_URL = 'https://en.wikipedia.org/wiki/Liam_Neeson'
 
 
 def main():
-    html_soup = get_html_soup(url=LIAM_NEESON_WIKIPEDIA_URL)
-    person = get_person(html_soup)
+    html_soup = get_html_soup(LIAM_NEESON_WIKIPEDIA_URL)
+    full_name = get_full_name(html_soup)
     #write_html(html_soup, output_file_name='pretty_liam.txt')
+    print(print_roles(html_soup))
     #liam_neeson_is_an_actor = is_an_actor(html_soup)
     #print('Is Liam Neeson an actor?!?! ' +
     #      'YES!' if liam_neeson_is_an_actor else 'no...')
@@ -28,12 +28,41 @@ def write_html(soup, output_file_name):
         file.write(pretty_html)
 
 
-def get_first_p_tag(html_soup):
-    return html_soup.p
+def get_first_p_tag(soup):
+    return soup.p
     
-#def is_an_actor(p_tag):
-    #check first sentence for "actor"
-    #return boolean
+def get_first_p_tag_text(soup):
+    first_p_tag = get_first_p_tag(soup)
+    return first_p_tag.get_text().lower()
+    
+def is_an_actor(soup):
+    text = get_first_p_tag_text(soup)
+    if('actor' in text or 'actress' in text):
+        return True
+    else:
+        return False
+        
+def is_a_director(soup):
+    text = get_first_p_tag_text(soup)
+    if('director' in text or 'filmmaker' in text):
+        return True
+    else:
+        return False
+        
+def is_a_composer(soup):
+    text = get_first_p_tag_text(soup)
+    if('composer' in text):
+        return True
+    else:
+        return False
+
+#improve to include director/composer, a/an, male/female
+def print_roles(soup):
+    name = get_person(soup)
+    if(is_an_actor(soup)):
+        print(name + ' is an actor.')
+    else:
+        print(name + ' is not an actor.')
 
 def get_person(soup):
     title_string = soup.title.string
@@ -41,6 +70,11 @@ def get_person(soup):
     person = title_string[:endIndex]
     print(person)
     return person
+    
+#Needs improvement with edge cases like quotes in bold tags (Babe Ruth)   
+def get_full_name(soup):
+    print(soup.b.string)
+    return soup.b.string
 
 if __name__ == '__main__':
     main()
